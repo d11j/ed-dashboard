2750b8923b2965b410b8cbfea1fc2bdde7fb385b
d11j <d11j@d11j.dev>
2026-07-02 19:58:52 +0900

    feat: PSスクリプトでenvファイルの存在確認と新規作成をするように修正

--- start-server.ps1
+++ start-server.ps1
@@ -11,6 +11,12 @@
 Write-Host "依存関係をインストールしています..." -ForegroundColor Green
 npm install
 
+if (-not (Test-Path -Path '.env')) {
+    Copy-Item -Path '.env.example' -Destination '.env'
+    Write-Host ".envファイルを作成しました。" -ForegroundColor Green
+    Write-Host "必要に応じて .env ファイルを編集して OBS_WEBSOCKET_PASSWORD を設定してください。" -ForegroundColor Yellow
+}
+
 Write-Host "サーバーを起動しています... (停止するには Ctrl+C を押してください)" -ForegroundColor Green
 # package.jsonのstartスクリプトを実行
 npm start
